a2b80d339403e0c080ac12f7f27ab20fd931bcd9
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-04-02 00:43:55 +0700

    Pengambilan data pada firestore dan ditampilkan pada recycleview

--- app/src/main/java/com/azhara/firebasefirestore/MainActivity.kt
+++ app/src/main/java/com/azhara/firebasefirestore/MainActivity.kt
@@ -2,11 +2,44 @@ package com.azhara.firebasefirestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.azhara.firebasefirestore.adapter.DiaryAdapter
+import com.azhara.firebasefirestore.model.DiaryEntity
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diaryAdapter: DiaryAdapter
+    private val db = FirebaseFirestore.getInstance() //Inisialisasi firestore
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diaryAdapter = DiaryAdapter()
+        getDataDiary()
+        with(rv_diary){
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = diaryAdapter
+        }
+    }
+
+    fun getDataDiary(){
+        val data = db.collection("diary")
+        val dataDiary = ArrayList<DiaryEntity>()
+        data.get()
+            .addOnSuccessListener { result ->
+                for (data in result){
+                    val diary = data.toObject(DiaryEntity::class.java)
+                    diary.id = data.id
+                    dataDiary.addAll(listOf(diary))
+                    Log.d("Activity", "$diary")
+                }
+                diaryAdapter.setData(dataDiary)
+            }
+
     }
 }
